81d88e12f157e80701ff62c022526261242a0b43
Daiana Monteiro <dtsilva.paiva@gmail.com>
2025-09-16 13:12:21 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nome = "";
+
+//salvar nome
+function salvarNome() {
+   nome = document.getElementById("nome").value;
+    if (nome == "") {
+        alert("Por favor, insira um nome válido");
+    } else {
+        listaDeAmigos.push(nome);
+        document.getElementById("nome").value = "";
+        atualizarLista();
+    }
+}
+
